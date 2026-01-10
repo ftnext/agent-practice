7218539773381b2d7760f168e90cd4ae6c2be825
ftnext <takuyafjp+develop@gmail.com>
2026-01-10 19:50:57 +0900

    fix: indent

--- agentengine/tests/integration/test_search_agent.py
+++ agentengine/tests/integration/test_search_agent.py
@@ -4,7 +4,7 @@ from google.adk.evaluation import AgentEvaluator
 
 @pytest.mark.asyncio
 async def test_should_use_search_tool():
-     await AgentEvaluator.evaluate(
+    await AgentEvaluator.evaluate(
         agent_module="search_agent",
         eval_dataset_file_path_or_dir="tests/integration/fixtures/search_agent/test_should_use_search_tool",
         num_runs=1,

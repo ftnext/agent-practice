818ea30d747cd905321f756be1ee95f950d3315b
ftnext <takuyafjp+develop@gmail.com>
2026-01-18 22:03:24 +0900

    feat: Agent can change frontend theme color

--- ag-ui/my-copilot-app/app/page.tsx
+++ ag-ui/my-copilot-app/app/page.tsx
@@ -1,8 +1,31 @@
-import { CopilotSidebar } from "@copilotkit/react-ui";
+"use client";
+import { useFrontendTool } from "@copilotkit/react-core";
+import { CopilotKitCSSProperties, CopilotSidebar } from "@copilotkit/react-ui";
+import { useState } from "react";
 
 export default function Page() {
+  const [themeColor, setThemeColor] = useState("#6366f1");
+
+  useFrontendTool({
+    name: "set_theme_color",
+    parameters: [
+      {
+        name: "theme_color",
+        description: "The theme color to set. Make sure to pick nice colors.",
+        required: true,
+      },
+    ],
+    handler({ theme_color }) {
+      setThemeColor(theme_color)
+    }
+  })
+
   return (
-    <main>
+    <main
+      style={
+        { "--copilot-kit-primary-color": themeColor } as CopilotKitCSSProperties
+      }
+    >
       <h1>Your App</h1>
       <CopilotSidebar />
     </main>

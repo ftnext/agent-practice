9242f22428ba496cf75a17a2eb467964094477ed
ftnext <takuyafjp+develop@gmail.com>
2026-03-07 18:32:54 +0900

    chore: Use latest model

--- agentengine/search_agent/agent.py
+++ agentengine/search_agent/agent.py
@@ -3,7 +3,7 @@ from google.adk.apps import App
 from google.adk.tools import google_search
 
 search_agent = Agent(
-    model='gemini-3-pro-preview',
+    model='gemini-3.1-pro-preview',
     name='search_agent',
     description='A helpful assistant that can search Google.',
     instruction="""\
